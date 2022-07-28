d107d32111753dd63e2c6ff94dbf86d4fa5c9a95
Gabi <gabi.beyo@imperva.com>
2022-07-28 14:48:59 +0300

    progress

--- hub/deploy/infra.tf
+++ hub/deploy/infra.tf
@@ -78,3 +78,40 @@ resource "aws_security_group_rule" "public_in_https" {
   security_group_id = aws_security_group.public.id
 }
  
+ resource "aws_eip" "lb" {
+  instance = aws_instance.web.id
+  vpc      = true
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.hub_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.public.id
+  network_interface_id = aws_instance.web.primary_network_interface_id
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.hub_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+
+  tags = {
+    Name = "example"
+  }
+}
+
+
+
