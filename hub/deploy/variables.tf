895f0d9314c309703b3acfc5ba2cc0ea4855685b
Eytan Naim <eytan.naim@imperva.com>
2022-08-01 10:36:54 +0300

    vars | Complete map of ami per region

--- hub/deploy/variables.tf
+++ hub/deploy/variables.tf
@@ -17,8 +17,27 @@ variable "hub_ami_id" {
 variable "hub_amis_id" {
   type = map(any)
   default = {
-    eu-west-2 = "ami-013984d976f6d6894"
-    eu-west-1 = "ami-065ec1e661d619058"
+    af-south-1 = "ami-064aeb2452a82205f"
+    ap-east-1 = "ami-0e377c3509b0a1490"
+    ap-northeast-1 = "ami-0073b6113281aa32e"
+    ap-northeast-2 = "ami-04adf7ab262061270"
+    ap-northeast-3 = "ami-0ea15419fc3544d6a"
+    ap-south-1 = "ami-01dd6bbea8d062bf3"
+    ap-southeast-1 = "ami-0ee6bfca4452064bc"
+    ap-southeast-2 = "ami-0bf7d72cc0b127581"
+    ca-central-1 = "ami-0f1c453d5ca1059f0"
+    eu-central-1 = "ami-0a62e33b5dcf31c85"
+    eu-north-1 = "ami-0a32f8cdd314cf493"
+    eu-south-1 = "ami-0f18d0a883583eaa9"
+    eu-west-1 = "ami-06211bde2f9c725e5"
+    eu-west-2 = "ami-0a8aebd46fb700315"
+    eu-west-3 = "ami-07185297899bb7755"
+    me-south-1 = "ami-02f76563b3f70c99d"
+    sa-east-1 = "ami-08acb85e5164523d7"
+    us-east-1 = "ami-004fac3d4533a2541"
+    us-east-2 = "ami-0bb2449c2217cb9b0"
+    us-west-1 = "ami-085733fd69e77a079"
+    us-west-2 = "ami-027da4fca766221c9"
   }
 }
 
