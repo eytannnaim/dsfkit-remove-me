d95161509c41f46a40350f93e30d080ea6ab8418
Eytan Naim <eytan.naim@imperva.com>
2022-08-10 08:34:54 +0300

    Use the right ssh key

--- hub/deploy/federate.sh
+++ hub/deploy/federate.sh
@@ -13,6 +13,6 @@ while ! nc -z -v ${dsf_gw_ip} 8443 &>/dev/null; do
     sleep 5
 done
 sleep 60
-ssh -o "StrictHostKeyChecking no" -i ~/dsf_hub_ssh_key.pem ec2-user@${dsf_hub_ip} -C 'sudo $JSONAR_BASEDIR/bin/federated warehouse '${dsf_hub_ip}' '${dsf_gw_ip}
-ssh -o "StrictHostKeyChecking no" -i ~/dsf_hub_ssh_key.pem ec2-user@${dsf_gw_ip} -C 'sudo $JSONAR_BASEDIR/bin/federated remote'
+ssh -o "StrictHostKeyChecking no" -i dsf_hub_ssh_key ec2-user@${dsf_hub_ip} -C 'sudo $JSONAR_BASEDIR/bin/federated warehouse '${dsf_hub_ip}' '${dsf_gw_ip}
+ssh -o "StrictHostKeyChecking no" -i dsf_hub_ssh_key ec2-user@${dsf_gw_ip} -C 'sudo $JSONAR_BASEDIR/bin/federated remote'
 
