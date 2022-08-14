916a493082b8d06db7ea2b49a6d319c92dd657a5
Eytan Naim <eytan.naim@imperva.com>
2022-08-14 08:45:12 +0300

    Update federate.sh

--- deploy/modules/gw/federate.sh
+++ deploy/modules/gw/federate.sh
@@ -12,7 +12,9 @@ while ! nc -z -v ${dsf_gw_ip} 8443 &>/dev/null; do
     echo "waiting for gw to go up"
     sleep 5
 done
-sleep 60
+
+# This sleep is a WA for federetion bug. This should be replaced with some grep on the sonar logs (intallation files)
+sleep 300
 
 ssh -o "StrictHostKeyChecking no" -i dsf_hub_ssh_key ec2-user@${dsf_hub_ip} -C 'sudo /opt/sonar-dsf/jsonar/apps/*/bin/federated warehouse '${dsf_hub_ip}' '${dsf_gw_ip}
 ssh -o "StrictHostKeyChecking no" -i dsf_hub_ssh_key ec2-user@${dsf_gw_ip} -C 'sudo /opt/sonar-dsf/jsonar/apps/*/bin/federated remote'
