d4cd18cdda5056b8547d593c3a14d2c2b35dc196
Eytan Naim <eytan.naim@imperva.com>
2022-08-18 11:07:18 +0300

    Change few things

--- deploy/modules/gw/federate.sh
+++ deploy/modules/gw/federate.sh
@@ -3,7 +3,7 @@ set -e
 
 dsf_hub_ip=$1
 dsf_gw_ip=$2
-
+ssh_key_path=ssh_keys/dsf_hub_ssh_key
 while ! nc -z -v ${dsf_hub_ip} 8443 &>/dev/null; do
     echo "waiting for hub to go up"
     sleep 5
@@ -16,7 +16,7 @@ done
 # This sleep is a WA for federetion bug. This should be replaced with some grep on the sonar logs (intallation files)
 sleep 120
 
-ssh -o StrictHostKeyChecking="no" -i dsf_hub_ssh_key ec2-user@${dsf_hub_ip} << HERE
+ssh -o StrictHostKeyChecking="no" -i ${ssh_key_path} ec2-user@${dsf_hub_ip} << HERE
 while [ -f sync_file ]; do
     sleep 5;
 done
@@ -25,4 +25,4 @@ sudo /opt/sonar-dsf/jsonar/apps/*/bin/federated warehouse '${dsf_hub_ip}' '${dsf
 rm sync_file
 HERE
 
-ssh -o StrictHostKeyChecking="no" -o ProxyCommand="ssh -i dsf_hub_ssh_key -W %h:%p ec2-user@${dsf_hub_ip}" -i dsf_hub_ssh_key ec2-user@${dsf_gw_ip} -C 'sudo /opt/sonar-dsf/jsonar/apps/*/bin/federated remote'
+ssh -o StrictHostKeyChecking="no" -o ProxyCommand="ssh -i ${ssh_key_path} -W %h:%p ec2-user@${dsf_hub_ip}" -i ${ssh_key_path} ec2-user@${dsf_gw_ip} -C 'sudo /opt/sonar-dsf/jsonar/apps/*/bin/federated remote'
