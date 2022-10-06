15f89b599ee8330b04e98147149ddcb4c669b0fe
Brian Anderson <brian.anderson@t470-173092.imperva.local>
2022-10-06 14:39:33 -0700

    updated octo-ba to include examples to onboard databases

--- deploy_cto_office/modules/SonarW/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public Elastic IP address of the DSF base instance"
-  value       = module.hub_instance.public_ip
-}
-
-output "private_ip" {
-  description = "Private IP address of the DSF base instance"
-  value       = module.hub_instance.private_ip
-}
